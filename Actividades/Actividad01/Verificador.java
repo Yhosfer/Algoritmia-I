5c2eaf44b9123aef3479bec1fa4785a1dc452247
luispozucsm <luis.pozu@ucsm.edu.pe>
2025-03-12 17:30:28 -0500

    Update Verificador.java

--- Actividades/Actividad01/Verificador.java
+++ Actividades/Actividad01/Verificador.java
@@ -8,7 +8,7 @@ public class Verificador {
 
     // Comentarios
     public static void calcularVars(Rectangulo r1) {
-        // Condicional se cumple si x1 es menos que x2 ???
+        // Este metodo esta hecho para ordenar el menor y mayor valor de cada esquina X y Y para facilitar el uso de los demas metodos
         if (r1.getEsquina1().getX() < r1.getEsquina2().getX()) {
             menorX = (double) r1.getEsquina1().getX();
             mayorX = (double) r1.getEsquina2().getX();
@@ -29,6 +29,7 @@ public class Verificador {
 
 
     public static boolean esSobrePos(Rectangulo r1, Rectangulo r2) {
+//Este metodo ve si el valor de una de las esquinas del segundo rectangulo estan entre los apartados menor y mayor del primero y saber si esta sobre este
         calcularVars(r1);
 
         if (r2.getEsquina1().getX() > menorX && r2.getEsquina1().getX() < mayorX) {
@@ -46,7 +47,7 @@ public class Verificador {
 
     public static boolean esJunto(Rectangulo r1, Rectangulo r2) {
         calcularVars(r1);
-
+    //este metodo lo que hace es comparar que la X o Y de un rectangulo este en la misma posición que el otro
         if (menorX == (double) r2.getEsquina1().getX() || menorX == (double) r2.getEsquina2().getX()) {
             return true;
         } else if (mayorX == (double) r2.getEsquina1().getX() || mayorX == (double) r2.getEsquina2().getX()) {
