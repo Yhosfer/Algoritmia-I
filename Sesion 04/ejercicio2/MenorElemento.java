825d981460b54315275e44b2743daa79c7ac27c0
luispozucsm <luis.pozu@ucsm.edu.pe>
2025-04-09 12:15:14 -0500

    Update MenorElemento.java

--- Sesion 04/ejercicio2/MenorElemento.java
+++ Sesion 04/ejercicio2/MenorElemento.java
@@ -1,24 +1,27 @@
 public class MenorElemento {
 
+    // Método principal que ordena y luego selecciona el k-ésimo elemento
     public static int seleccionarElemento(int[] arreglo, int k) {
-        return quickSelect(arreglo, 0, arreglo.length - 1, k - 1);
+        quicksort(arreglo, 0, arreglo.length - 1);
+        
+        // Luego, simplemente devolvemos el elemento en la posición k-1
+        return arreglo[k - 1];
     }
 
-    private static int quickSelect(int[] arreglo, int izquierda, int derecha, int indiceBuscado) {
-        if (izquierda == derecha) return arreglo[izquierda];
-
-        int indicePivote = particionar(arreglo, izquierda, derecha);
-
-        if (indiceBuscado == indicePivote)
-            return arreglo[indiceBuscado];
-        else if (indiceBuscado < indicePivote)
-            return quickSelect(arreglo, izquierda, indicePivote - 1, indiceBuscado);
-        else
-            return quickSelect(arreglo, indicePivote + 1, derecha, indiceBuscado);
+    // Método Quicksort para ordenar el arreglo
+    private static void quicksort(int[] arreglo, int izquierda, int derecha) {
+        if (izquierda < derecha) {
+            int indicePivote = particionar(arreglo, izquierda, derecha);
+            
+            // Ordenamos recursivamente las dos mitades
+            quicksort(arreglo, izquierda, indicePivote - 1);
+            quicksort(arreglo, indicePivote + 1, derecha);
+        }
     }
 
+    // Partición (igual que en el código original)
     private static int particionar(int[] arreglo, int izquierda, int derecha) {
-        int valorPivote = arreglo[derecha]; // Pivote fijo: el último elemento
+        int valorPivote = arreglo[derecha];
         int indiceAlmacenamiento = izquierda;
 
         for (int i = izquierda; i < derecha; i++) {
@@ -32,10 +35,10 @@ public class MenorElemento {
         return indiceAlmacenamiento;
     }
 
+    // Método auxiliar para intercambiar elementos (igual que antes)
     private static void intercambiar(int[] arreglo, int i, int j) {
         int temp = arreglo[i];
         arreglo[i] = arreglo[j];
         arreglo[j] = temp;
     }
 }
-
