30930c357fadfefac460371f6e68eaf1776baec3
luispozucsm <luis.pozu@ucsm.edu.pe>
2025-04-08 19:27:22 -0500

    Update MenorElemento.java

--- Sesion 04/ejercicio2/MenorElemento.java
+++ Sesion 04/ejercicio2/MenorElemento.java
@@ -44,11 +44,4 @@ public class MenorElemento {
         arreglo[j] = temp;
     }
 
-    // Pruebas
-    public static void main(String[] args) {
-        System.out.println(seleccionarElemento(new int[]{4, 2, 7, 10, 4, 17}, 3)); // 4
-        System.out.println(seleccionarElemento(new int[]{4, 2, 7, 10, 4, 1, 6}, 5)); // 6
-        System.out.println(seleccionarElemento(new int[]{4, 2, 7, 1, 4, 6}, 1)); // 1
-        System.out.println(seleccionarElemento(new int[]{9, 2, 7, 1, 7}, 4)); // 7
-    }
-}
+
