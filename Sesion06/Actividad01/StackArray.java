88348cf11247e9c60004fcecb9f1b011319ea4ae
luispozucsm <luis.pozu@ucsm.edu.pe>
2025-05-04 18:44:27 -0500

    Create StackArray.java

---
+++ Sesion06/Actividad01/StackArray.java
@@ -0,0 +1,43 @@
+public class StackArray<E> implements Stack<E> {
+    private E[] array;
+    private int tope;
+
+    public StackArray(int n) {
+        this.array = (E[]) new Object[n];
+        tope = -1;
+    }
+
+    public void push(E x) {
+        if (isFull()) {
+            System.out.println("Stack lleno.");
+            return;
+        }
+        array[++tope] = x;
+    }
+
+    public E pop() throws ExceptionIsEmpty {
+        if (isEmpty()) throw new ExceptionIsEmpty("Stack vacío.");
+        return array[tope--];
+    }
+
+    public E top() throws ExceptionIsEmpty {
+        if (isEmpty()) throw new ExceptionIsEmpty("Stack vacío.");
+        return array[tope];
+    }
+
+    public boolean isEmpty() {
+        return tope == -1;
+    }
+
+    public boolean isFull() {
+        return tope == array.length - 1;
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder("Stack: ");
+        for (int i = tope; i >= 0; i--) {
+            sb.append(array[i]).append(" ");
+        }
+        return sb.toString();
+    }
+}
