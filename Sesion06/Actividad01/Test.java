f3b20669fafc7bcd8c5cf803976fd4d59d16e374
luispozucsm <luis.pozu@ucsm.edu.pe>
2025-05-04 22:18:41 -0500

    Add files via upload

---
+++ Sesion06/Actividad01/Test.java
@@ -0,0 +1,25 @@
+public class Test {
+    public static void main(String[] args) {
+
+        Stack<String> pila = new StackArray<>(4);
+
+        pila.push("uno");
+        pila.push("dos");
+        pila.push("tres");
+
+        System.out.println(pila);
+
+        try {
+            System.out.println("Top: " + pila.top()); // tres
+            System.out.println("Pop: " + pila.pop()); // tres
+            System.out.println("Top después de pop: " + pila.top()); // dos
+        } catch (ExceptionIsEmpty e) {
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println("¿Está vacía?: " + pila.isEmpty());
+        pila.push("cuatro");
+        pila.push("cinco");
+        System.out.println(pila); 
+    }
+}
